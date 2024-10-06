40af242aacdda84a06769a80b11e1c8cb952ef69
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-10-05 22:59:48 -0300

    resolvido 3 metodo

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import entities.Product;
@@ -16,8 +17,19 @@ public class Program {
 		list.add(new Product("Notebook", 1200.00));
 		list.add(new Product("Tablet", 450.00));
 		
+		Comparator<Product> comp = new Comparator<Product>() {
+
+			@Override
+			public int compare(Product p1, Product p2) {
+				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+			}
+			
+		};
+		
+		list.sort(comp);
+		
 		// Collections.sort(list); // Tem que ser o comparable para usar o sort
-		list.sort(new MyComparator());
+		// list.sort(new MyComparator());
 		
 		for(Product p : list) {
 			System.out.println(p);
